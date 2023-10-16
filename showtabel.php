82babe67ff9a4b1589ba9a1e83577f390fb89d38
Natania Setyawati <natania.nn64@gmail.com>
2023-10-16 20:45:15 +0800

    perubahan di showtabel

--- showtabel.php
+++ showtabel.php
@@ -4,57 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Form Biodata</title>
-    <style>
-
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f0f0f0;
-        margin: 0;
-        padding: 0;
-      }
-
-      h1 {
-        text-align: center;
-        color: #333;
-      }
-
-      form {
-        max-width: 400px;
-        margin: 0 auto;
-        padding: 20px;
-        background-color: #fff;
-        border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      }
-
-      label {
-        font-weight: bold;
-      }
-
-      input[type='text'],
-      input[type='tel'],
-      input[type='email'],
-      select {
-        width: 100%;
-        padding: 8px;
-        margin-bottom: 10px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-      }
-
-      input[type='submit'] {
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        padding: 10px 20px;
-        cursor: pointer;
-      }
-
-      input[type='submit']:hover {
-        background-color: #0056b3;
-      }
-    </style>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <h1>Hasil Biodata</h1>
