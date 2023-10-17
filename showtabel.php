2d9b0989b8b9653cbab3741abc303ef56cf4101e
Natania Setyawati <natania.nn64@gmail.com>
2023-10-17 18:25:31 +0800

    another new style

--- showtabel.php
+++ showtabel.php
@@ -5,58 +5,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Form Biodata</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+
     <style>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f0f0f0;
-        margin: 0;
-        padding: 0;
-      }
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  margin: 0;
+  padding: 0;
+}
 
-      h1 {
-        text-align: center;
-        color: #333;
-      }
+form {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
 
-      form {
-        max-width: 400px;
-        margin: 0 auto;
-        padding: 20px;
-        background-color: #fff;
-        border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      }
+h1 {
+  text-align: center;
+  color: #333;
+}
 
-      label {
-        font-weight: bold;
-      }
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 5px;
+}
 
-      input[type='text'],
-      input[type='tel'],
-      input[type='email'],
-      select {
-        width: 100%;
-        padding: 8px;
-        margin-bottom: 10px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-      }
+input[type='text'],
+input[type='tel'],
+input[type='email'],
+select {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
 
-      input[type='submit'] {
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        padding: 10px 20px;
-        cursor: pointer;
-      }
+input[type='submit'] {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
 
-      input[type='submit']:hover {
-        background-color: #0056b3;
-      }
-    </style>
-    </style>
+input[type='submit']:hover {
+  background-color: #0056b3;
+}
+
+</style>
+
 </head>
 <body>
     <h1>Hasil Biodata</h1>
