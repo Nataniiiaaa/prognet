7fc2d130b6da7f68b7ba16986bdbf8cf5fa15975
Natania Setyawati <natania.nn64@gmail.com>
2023-10-18 00:52:19 +0800

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,13 @@
-$host = '192.168.4.4';
-$dbname = 'db_2205551001';
-$username = '2205551001';
-$password = '2205551001';
+<?php
+$host = '192.168.4.4'; 
+$dbname = 'db_2205551001'; 
+$username = '2205551001'; 
+$password = '2205551001'; 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Aktifkan penanganan kesalahan
-        PDO::ATTR_EMULATE_PREPARES => false, // Matikan penggantian parameter
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Mengambil hasil dalam bentuk array asosiatif
-    ));
+    $koneksi = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Atur opsi lainnya jika diperlukan
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
 }
+?>
