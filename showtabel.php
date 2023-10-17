fe72b76e9e0423f6c000ea7a42b73063b6bbb98f
Natania Setyawati <natania.nn64@gmail.com>
2023-10-17 18:22:46 +0800

    new style

--- showtabel.php
+++ showtabel.php
@@ -6,58 +6,56 @@
     <title>Form Biodata</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <style>
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f0f0f0;
-    margin: 0;
-    padding: 0;
-}
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f0f0f0;
+        margin: 0;
+        padding: 0;
+      }
 
-h1 {
-    text-align: center;
-    color: #333;
-}
+      h1 {
+        text-align: center;
+        color: #333;
+      }
 
-#result {
-    max-width: 600px;
-    margin: 20px auto;
-    padding: 20px;
-    background-color: #fff;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
+      form {
+        max-width: 400px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
 
-.form-table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-bottom: 20px;
-    border: 1px solid #ccc; /* Border for the whole table */
-}
+      label {
+        font-weight: bold;
+      }
 
-.form-table th, .form-table td {
-    border: 1px solid #ccc; /* Border for table cells */
-    padding: 8px;
-    text-align: left;
-}
+      input[type='text'],
+      input[type='tel'],
+      input[type='email'],
+      select {
+        width: 100%;
+        padding: 8px;
+        margin-bottom: 10px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+      }
 
-.form-table th {
-    background-color: #f2f2f2;
-}
+      input[type='submit'] {
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        border-radius: 4px;
+        padding: 10px 20px;
+        cursor: pointer;
+      }
 
-#result p {
-    margin: 0;
-    padding: 0;
-}
-
-#result strong {
-    font-weight: bold;
-}
-
-#result h2 {
-    text-align: center;
-    color: #333;
-}
-
+      input[type='submit']:hover {
+        background-color: #0056b3;
+      }
+    </style>
     </style>
 </head>
 <body>
