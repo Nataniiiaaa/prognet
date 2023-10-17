821e9170db1b5adab81053338882aae7a9de8f53
Natania Setyawati <natania.nn64@gmail.com>
2023-10-18 01:33:05 +0800

    change

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php
-$host = '192.168.4.4'; 
+$server = '192.168.4.4'; 
 $dbname = 'db_2205551001'; 
 $username = '2205551001'; 
 $password = '2205551001'; 
 
-$conn = new mysqli($host, $dbname, $username, $password);
+$conn = new mysqli($server, $dbname, $username, $password);
 if ($conn->connect_error) {
     die("Connection Failed: ". $conn->connect_error);
 }
