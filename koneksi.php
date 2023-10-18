5f80db861e467bdcb37841d303791363833ed598
Natania Setyawati <natania.nn64@gmail.com>
2023-10-18 10:34:12 +0800

    new

--- koneksi.php
+++ koneksi.php
@@ -2,9 +2,9 @@
 $host = 'prognet.localnet'; 
 $username = '2205551001'; 
 $password = '2205551001'; 
-$dbname = 'db_2205551001'; 
+$database = 'db_2205551001'; 
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
