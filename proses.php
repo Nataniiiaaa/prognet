ef84d70ed4b339c67604f939afbb36fb2846e0a7
Natania Setyawati <natania.nn64@gmail.com>
2023-10-18 09:29:05 +0800

    change

--- proses.php
+++ proses.php
@@ -1,36 +1,36 @@
 <?php
-// Include file koneksi.php untuk menghubungkan ke database
-include('koneksi.php');
+// Pastikan Anda sudah memasukkan file koneksi.php yang berisi konfigurasi koneksi ke database
+include 'koneksi.php';
 
-// Ambil data dari formulir
-$name = $_POST['name'];
-$nim = $_POST['nim'];
-$birthdate = $_POST['birthdate'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$address = $_POST['address'];
-$gender = $_POST['gender'];
-$status = $_POST['status'];
-$major = $_POST['major'];
-$campus = $_POST['campus'];
-$hobbies = $_POST['hobbies'];
-$food = implode(', ', $_POST['food']); // Menggabungkan array makanan menjadi string
-$drink = implode(', ', $_POST['drink']); // Menggabungkan array minuman menjadi string
-$favoriteColor = $_POST['favorite-color'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $nim = $_POST["nim"];
+    $birthdate = $_POST["birthdate"];
+    $phone = $_POST["phone"];
+    $email = $_POST["email"];
+    $address = $_POST["address"];
+    $gender = $_POST["gender"];
+    $status = $_POST["status"];
+    $major = $_POST["major"];
+    $campus = $_POST["campus"];
+    $hobbies = implode(", ", $_POST["hobbies"]);
+    $favoriteFoods = implode(", ", $_POST["food"]);
+    $favoriteDrinks = implode(", ", $_POST["drink"]);
+    $favoriteColor = $_POST["favorite-color"];
 
-// Query SQL untuk menambahkan data ke tabel 'biodata'
-$query = "INSERT INTO biodata (NAME, nim, birthdate, phone, email, address, gender, status_user, major, campus, hobbies, favorite_foods, favorite_drinks, favorite_color) 
-VALUES ('$name', '$nim', '$birthdate', '$phone', '$email', '$address', '$gender', '$status', '$major', '$campus', '$hobbies', '$food', '$drink', '$favoriteColor')";
+    // SQL statement untuk memasukkan data ke dalam tabel biodata
+    $sql = "INSERT INTO biodata (NAME, nim, birthdate, phone, email, address, gender, status_user, major, campus, hobbies, favorite_foods, favorite_drinks, favorite_color)
+            VALUES ('$name', '$nim', '$birthdate', '$phone', '$email', '$address', '$gender', '$status', '$major', '$campus', '$hobbies', '$favoriteFoods', '$favoriteDrinks', '$favoriteColor')";
 
-// Eksekusi query
-$result = mysqli_query($koneksi, $query);
+    if ($conn->query($sql) === TRUE) {
+        echo "Data berhasil disimpan.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-if ($result) {
-    echo "Data berhasil disimpan.";
+    // Tutup koneksi ke database
+    $conn->close();
 } else {
-    echo "Data gagal disimpan. Error: " . mysqli_error($koneksi);
+    echo "Data belum dikirim. Silakan isi formulir terlebih dahulu.";
 }
-
-// Tutup koneksi database
-mysqli_close($koneksi);
 ?>
