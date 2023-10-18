372d06a6cc3e3250d2f9c2787ceac6a1ed6c4806
Natania Setyawati <natania.nn64@gmail.com>
2023-10-18 09:06:03 +0800

    change

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php
-$server = 'prognet.localnet'; 
+$host = 'prognet.localnet'; 
 $dbname = 'db_2205551001'; 
 $username = '2205551001'; 
 $password = '2205551001'; 
 
-$conn = new mysqli($server, $dbname, $username, $password);
+$conn = new mysqli($host, $dbname, $username, $password);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
